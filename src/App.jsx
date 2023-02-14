b632a695f61a176cdc47e2a90c6505711810f63c
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-02-14 22:19:47 +0530

    update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./app.css"
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (<>
+        <Navbar />
         <h1>GOOGLE AUTHENTICATION</h1>
     </>)
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css"
+import Avatar from "../img/avatar.png"
+
+
+const Navbar = () => {
+    return (<>
+        <div className="navbar">
+            <span className="logo">Dev.R</span>
+            <ul className="list">
+                <li className="listItem">
+                    <img src={Avatar} alt="avatar" className="avatar" />
+                </li>
+                <li className="listItem">Roshitha</li>
+                <li className="listItem">Logout</li>
+            </ul>
+        </div>
+    </>)
+}
+
+export default Navbar
