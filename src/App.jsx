68a35af99af2b44eb1cf1f6de6d9672a4c70ef6e
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-02-14 23:12:47 +0530

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./app.css"
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import "./app.css"
 
 const App = () => {
     return (<>
         <Navbar />
+        <Home />
         <h1>GOOGLE AUTHENTICATION</h1>
     </>)
 }
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const posts = [
+    {
+        id: 1,
+        title: "5 Premium theme for react",
+        img: "./img/blog.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam.",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam. Ut blandit in tellus vitae malesuada. Nulla sodales sed leo vel ullamcorper. Donec tincidunt ligula turpis, blandit tincidunt dui tempor id. Aliquam cursus eros tellus, sed sodales elit tempor vel. Fusce ullamcorper imperdiet dui eget efficitur. Nulla ornare semper sapien, id dapibus risus consequat efficitur. Integer id ligula tristique nisl condimentum interdum ut et lectus. Praesent tristique, erat ut consectetur lacinia, tortor neque ultrices nibh, facilisis volutpat magna orci placerat neque."
+    },
+    {
+        id: 2,
+        title: "How to add elements?",
+        img: "./img/blog.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam.",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam. Ut blandit in tellus vitae malesuada. Nulla sodales sed leo vel ullamcorper. Donec tincidunt ligula turpis, blandit tincidunt dui tempor id. Aliquam cursus eros tellus, sed sodales elit tempor vel. Fusce ullamcorper imperdiet dui eget efficitur. Nulla ornare semper sapien, id dapibus risus consequat efficitur. Integer id ligula tristique nisl condimentum interdum ut et lectus. Praesent tristique, erat ut consectetur lacinia, tortor neque ultrices nibh, facilisis volutpat magna orci placerat neque."
+    },
+    {
+        id: 3,
+        title: "How to work with JavaScript",
+        img: "./img/blog.jpg",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam.",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum mollis, nulla eget suscipit vehicula, lectus lectus tempus quam, non bibendum erat dolor vel diam. Ut blandit in tellus vitae malesuada. Nulla sodales sed leo vel ullamcorper. Donec tincidunt ligula turpis, blandit tincidunt dui tempor id. Aliquam cursus eros tellus, sed sodales elit tempor vel. Fusce ullamcorper imperdiet dui eget efficitur. Nulla ornare semper sapien, id dapibus risus consequat efficitur. Integer id ligula tristique nisl condimentum interdum ut et lectus. Praesent tristique, erat ut consectetur lacinia, tortor neque ultrices nibh, facilisis volutpat magna orci placerat neque."
+    }
+]
