299a7cc69ff518da1eb51f705317f48012932b6a
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-02-16 23:43:41 +0530

    added passport js to project

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
+const cookieSession = require("cookie-session")
+const passport = require("passport")
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
+app.use(
+    cookieSession({name: "session", keys: ["Rosh"], maxAge: 24 * 60 * 60 * 100})
+)
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    credentials: true
+}))
 
 app.listen("5000", () => {
     console.log("Server running on 5000")
